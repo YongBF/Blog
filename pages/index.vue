e29d2e18c3da63894570c9dba10015eb9342b51a
YongBF <1727132050@qq.com>
2020-12-17 17:14:34 +0800

    init nuxt

---
+++ pages/index.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="container">
+    <div>
+      <Logo />
+      <h1 class="title">
+        {{title}}
+      </h1>
+      <p>{{body}}</p>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default {
+  data() {
+    return {
+      title: 'blog1',
+      body: 'default body'
+    }
+  },
+  async asyncData(context: {param: Object | undefined, req: any, res: Object}) {
+    const {param, req, res} = context
+    console.log('this is console', res)
+    let body = await new Promise((resovle) => {
+      setTimeout(() => {
+        resovle('this is body')
+      }, 1000);
+    })
+    return { body }
+  }
+}
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family:
+    'Quicksand',
+    'Source Sans Pro',
+    -apple-system,
+    BlinkMacSystemFont,
+    'Segoe UI',
+    Roboto,
+    'Helvetica Neue',
+    Arial,
+    sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
